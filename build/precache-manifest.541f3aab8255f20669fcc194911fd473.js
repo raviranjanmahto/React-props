028dad63e5da66da4157d96056f5bcaa94484a45
Raviranjan Mahto <rvrnjnmahto@gmail.com>
2021-11-19 10:19:34 +0530

    initial commit

---
+++ build/precache-manifest.541f3aab8255f20669fcc194911fd473.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8ba84633c02babbc7845dc72456c0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bf766714fd3ca51719e",
+    "url": "/static/js/2.4158ec86.chunk.js"
+  },
+  {
+    "revision": "03340275544785c9ed9a",
+    "url": "/static/js/main.ca18b93c.chunk.js"
+  },
+  {
+    "revision": "44105ad7c8f6d07310b3",
+    "url": "/static/js/runtime-main.a19405b8.js"
+  }
+]);
